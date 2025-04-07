13dc7dd7ac1fc6330d5d607b8e5728e720b135d5
Boochie123 <gokul.s01999@gmail.com>
2025-04-07 17:19:23 +0530

    updated code

--- Server/server.ts
+++ Server/server.ts
@@ -19,6 +19,9 @@ dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const port=process.env.PORT || 5000
 
+console.log('MongoDB URL:', process.env.MONGO_DB_URL || 'NOT FOUND'); // Debug
+
+
 if(process.env.MONGO_DB_URL!==undefined)
 {
     mongoose.connect(process.env.MONGO_DB_URL).then(()=>
